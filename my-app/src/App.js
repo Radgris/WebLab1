47aae398b745047215c39b3048403067ffd2f96e
Radgris <golden_run@hotmail.com>
2020-03-27 19:07:01 -0700

    2nd commit
    First and third point implemented

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,22 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const data =[
+    {id: 1, name: 'testA'},
+  ]
+  
+  const [displayvalues, SetValues] = React.useState([])
+
+  const getDatafromApi = async () => {
+    const apidata = await fetch('http://api.openweathermap.org/data/2.5/forecast?id=4014338&cnt=8&APPID=2104d38bbd22c6405f6a68ca788c2af0')
+    const dates = await apidata.json()
+    const parsed = dates.list
+    console.log(parsed)
+    //console.log('data')
+    //console.log(apidata)
+    SetValues(parsed)
+  }
+
+  React.useEffect(()=>{
+    //console.log('effectused')
+    var datafromapi = getDatafromApi()
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Forecast</h1>
+     <ul>
+        {displayvalues.map(item=>(
+          <li>{item.dt_txt}---{item.main.temp}---{item.main.humidity}</li>
+        ))
+        }
+     </ul>
     </div>
   );
 }
